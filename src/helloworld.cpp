e7269a9bb14bfbcb18e6f93015da8b5588a28ad4
yaxin liu <3028647549@qq.com>
2024-10-24 10:41:13 +0800

    实验

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 int main ()
-{
-    int year,mm,Days;
-    if(mm==1|mm==3|mm==5|mm==7|mm==8|mm==10|mm==12) Days =31;
-    else if(mm==4|mm==6|mm==9|mm==11) Days =30;
-    else (mm==2)
-    
+{    int x = 1, y = 0;
+   cout<< x || y;
 
 }
